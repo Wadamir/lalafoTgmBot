553b959ccaf28b304794b9a6a82e130490b78e54
Wadamir <domus159@gmail.com>
2023-10-22 19:11:45 +0600

    fix start

--- start.php
+++ start.php
@@ -101,6 +101,7 @@ if ($chat_type === 'message' && $user_data['is_bot'] === 0 && $message_type === 
                 // Send message
                 $bot = new \TelegramBot\Api\BotApi($token);
                 $user_result = createUser($user_data);
+                file_put_contents($start_log_file, ' | User result - ' . print_r($user_result, true), FILE_APPEND);
 
                 if ($user_result === true) {
                     // Get all cities
